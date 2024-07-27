0a85bd40d79113f902530024dba443d417a48a8b
Sidath Samarasinghe <sidaath@gmail.com>
2024-07-27 23:46:58 +0200

    clear and disable forms while waiting

--- app/lib/food/mocks.tsx
+++ app/lib/food/mocks.tsx
@@ -1,105 +1,154 @@
-'use server'
+"use server";
 
 import { revalidatePath } from "next/cache";
+import { resolve } from "path";
 /* GLOBAL Id STUFF*/
 let temp = 10;
 
 /* */
 
 /* Mocks for the to buy grocery list*/
-const item1 = {itemName:'Item 1', size:3, units:'Nos', quantity:3, id:'1'}
-const item2 = {itemName:'Item 2', size:7, units:'Nos', quantity:6, id:'2'}
-const item3 = {itemName:'Item 3', size:1, units:'kg', quantity: 2, id:'3'}
+const item1 = {
+  itemName: "Item 1",
+  size: 3,
+  units: "Nos",
+  quantity: 3,
+  id: "1",
+};
+const item2 = {
+  itemName: "Item 2",
+  size: 7,
+  units: "Nos",
+  quantity: 6,
+  id: "2",
+};
+const item3 = {
+  itemName: "Item 3",
+  size: 1,
+  units: "kg",
+  quantity: 2,
+  id: "3",
+};
 
 let toBuyItemsList = [item1, item2, item3];
 
-export async function addNewItemToBuy(formData: FormData){
-    console.log("ruinning addNewItemToBuy")
-    const newItem = {
-        itemName : formData.get('itemName')?.toString()||'',
-        size : Number(formData.get('size')),
-        units : formData.get('unit')?.toString() ||'',
-        quantity:Number(formData.get('quantity')),
-        id: temp.toString()
-    }
+export async function addNewItemToBuy(formData: FormData) {
+  console.log("ruinning addNewItemToBuy");
+  const newItem = {
+    itemName: formData.get("itemName")?.toString() || "",
+    size: Number(formData.get("size")),
+    units: formData.get("unit")?.toString() || "",
+    quantity: Number(formData.get("quantity")),
+    id: temp.toString(),
+  };
 
-    temp = temp + 1    
-    toBuyItemsList = [...toBuyItemsList, newItem]
-    revalidatePath('/food')
+  temp = temp + 1;
+  console.log("start wait");
+  const wait = (ms: number) =>
+    new Promise((resolve) => {
+      setTimeout(resolve, ms);
+    });
+
+  await new Promise((resolve) => setTimeout(resolve, 1000));
+  console.log("end wait");
+  toBuyItemsList = [...toBuyItemsList, newItem];
+  revalidatePath("/food");
 }
 
-export async function getToBuyList(){
-    console.log("running getToBuyList")
-    return toBuyItemsList;
+export async function getToBuyList() {
+  console.log("running getToBuyList");
+  return toBuyItemsList;
 }
 
-async function removeFromToBuy(itemId:string){
-    console.log("running removeFromToBuy")
-    const index = toBuyItemsList.findIndex(x => x.id===itemId)
-    toBuyItemsList.splice(index, 1)
+async function removeFromToBuy(itemId: string) {
+  console.log("running removeFromToBuy");
+  const index = toBuyItemsList.findIndex((x) => x.id === itemId);
+  toBuyItemsList.splice(index, 1);
 }
-
 
 /* Mocks for the inventory list*/
-const invItem1 = {itemName:'Inv Item 1', size:7, units:'Nos', quantity:12, id:'4', expiry:null}
-const invItem2 = {itemName:'Inv Item 2', size:14, units:'Nos', quantity:1, id:'5', expiry:'2024-10-12'}
-const invItem3 = {itemName:'Inv Item 3', size:3, units:'kg', quantity: 98, id:'6', expiry:null}
+const invItem1 = {
+  itemName: "Inv Item 1",
+  size: 7,
+  units: "Nos",
+  quantity: 12,
+  id: "4",
+  expiry: null,
+};
+const invItem2 = {
+  itemName: "Inv Item 2",
+  size: 14,
+  units: "Nos",
+  quantity: 1,
+  id: "5",
+  expiry: "2024-10-12",
+};
+const invItem3 = {
+  itemName: "Inv Item 3",
+  size: 3,
+  units: "kg",
+  quantity: 98,
+  id: "6",
+  expiry: null,
+};
 
-let inventory = [invItem1, invItem2, invItem3]
+let inventory = [invItem1, invItem2, invItem3];
 
-export async function getInventory(){
-    console.log("running getInventory")
-    return inventory;
+export async function getInventory() {
+  console.log("running getInventory");
+  return inventory;
 }
 
-export async function addToInventory(formData: FormData){
-    console.log("running addToInventory")
-    const newInvItem = {
-        itemName : formData.get('itemName')?.toString()||'',
-        size : Number(formData.get('size')),
-        units : formData.get('unit')?.toString() ||'',
-        quantity:Number(formData.get('quantity')),
-        id : formData.get('id')?.toString()||'',
-        expiry: formData.get('exp')?.toString()||null
-    }
-    
-    inventory = [...inventory,  newInvItem]
-    removeFromToBuy(newInvItem.id)
-    revalidatePath('/food')
+export async function addToInventory(formData: FormData) {
+  console.log("running addToInventory");
+  const newInvItem = {
+    itemName: formData.get("itemName")?.toString() || "",
+    size: Number(formData.get("size")),
+    units: formData.get("unit")?.toString() || "",
+    quantity: Number(formData.get("quantity")),
+    id: formData.get("id")?.toString() || "",
+    expiry: formData.get("exp")?.toString() || null,
+  };
 
+  await new Promise((resolve) => setTimeout(resolve, 1000));
+
+  inventory = [...inventory, newInvItem];
+  removeFromToBuy(newInvItem.id);
+  revalidatePath("/food");
 }
 
 export async function addToInvDirect(formData: FormData) {
-    console.log("running addToInvDirect")
-    const newInvItem = {
-        itemName : formData.get('itemName')?.toString()||'',
-        size : Number(formData.get('size')),
-        units : formData.get('unit')?.toString() ||'',
-        quantity:Number(formData.get('quantity')),
-        id : formData.get('id')?.toString()||'',
-        expiry: formData.get('exp')?.toString()||null
-    }
-    
-    inventory = [...inventory,  newInvItem]
-    revalidatePath('/food')
+  console.log("running addToInvDirect");
+  const newInvItem = {
+    itemName: formData.get("itemName")?.toString() || "",
+    size: Number(formData.get("size")),
+    units: formData.get("unit")?.toString() || "",
+    quantity: Number(formData.get("quantity")),
+    id: formData.get("id")?.toString() || "",
+    expiry: formData.get("exp")?.toString() || null,
+  };
 
+  await new Promise((resolve) => setTimeout(resolve, 1000));
+
+  inventory = [...inventory, newInvItem];
+  revalidatePath("/food");
 }
 
-export async function increment(itemId:string){
-    console.log("running increment")
-    const index = inventory.findIndex(x => x.id===itemId)
-    inventory[index].quantity = inventory[index].quantity + 1;
-    await new Promise((resolve) => setTimeout(resolve, 1000));
-    revalidatePath('/food')
+export async function increment(itemId: string) {
+  console.log("running increment");
+  const index = inventory.findIndex((x) => x.id === itemId);
+  inventory[index].quantity = inventory[index].quantity + 1;
+  await new Promise((resolve) => setTimeout(resolve, 1000));
+  revalidatePath("/food");
 }
 
-export async function decrement(itemId:string){
-    console.log("running decrement")
-    const index = inventory.findIndex(x => x.id===itemId)
-    inventory[index].quantity = inventory[index].quantity - 1;
-    if(inventory[index].quantity === 0){
-        inventory.splice(index, 1)
-    }
-    await new Promise((resolve) => setTimeout(resolve, 1000));
-    revalidatePath('/food')
+export async function decrement(itemId: string) {
+  console.log("running decrement");
+  const index = inventory.findIndex((x) => x.id === itemId);
+  inventory[index].quantity = inventory[index].quantity - 1;
+  if (inventory[index].quantity === 0) {
+    inventory.splice(index, 1);
+  }
+  await new Promise((resolve) => setTimeout(resolve, 1000));
+  revalidatePath("/food");
 }
--- app/ui/food/NewGroceryItem.tsx
+++ app/ui/food/NewGroceryItem.tsx
@@ -1,52 +1,77 @@
-import { addNewItemToBuy } from "@/app/lib/food/mocks"
+"use client";
 
-import AddToCartIcon from "../icons/addtocard"
+import { useRef, useTransition } from "react";
+import AddToCartIcon from "../icons/addtocard";
 
-export default async function NewItem({formHandler = (FormData: FormData) => {}}){
-    const units = ['kg', 'g', 'pack', 'L', 'mL', 'dL']
-    
-    return(
-        <form action={formHandler} className="flex flex-col lg:flex-row p-3 rounded gap-4 bg-green-200">
-            <input 
-                id="itemName" 
-                name="itemName" 
-                type="text" 
-                placeholder="add item ..." 
-                required={true}
-                className="md:w-2/3 rounded border border-gray-200 text-sm placeholder:text-gray-500 "/>
-            <input 
-                id="size"
-                name="size"
-                type="number"
-                placeholder="Size"
-                required={true}
-                className="w-20 ml-15 rounded-md border border-gray-200 p-1 text-sm placeholder:text-gray-500"/>
-            <select
-                id="unit"
-                name="unit"
-                className="w-20 rounded border border-gray-200 text-sm placeholder:text-gray-500"
-                required={true}
-                defaultValue=""
-                >
-                <option value="" disabled>
-                    select
-                </option>
-                {units.map((unit) => (
-                    <option key={unit} value={unit}>
-                        {unit}
-                    </option>
-                ))}
-            </select>
-            <input 
-                id="quantity"
-                name="quantity"
-                type="number"
-                placeholder="Qty"
-                required={true}
-                className="w-20 rounded-md border border-gray-200 text-sm placeholder:text-gray-500"/>
-            <button type="submit" className="w-1/12">
-                <AddToCartIcon className="stroke-slate-50 hover:bg-blue-300 w-10 h-8 rounded"/>
-            </button>
-        </form>
-    )
+export default function NewItem({ formHandler = (FormData: FormData) => {} }) {
+  const units = ["kg", "g", "pack", "L", "mL", "dL"];
+  const [saveTransition, startSveTransition] = useTransition();
+  const formRef = useRef<HTMLFormElement>(null);
+
+  async function submitForm(formData: FormData) {
+    startSveTransition(async () => {
+      await formHandler(formData);
+      formRef.current?.reset();
+    });
+  }
+
+  return (
+    <form
+      ref={formRef}
+      action={(formData) => submitForm(formData)}
+      className="flex flex-col lg:flex-row p-3 rounded gap-4 bg-green-200"
+    >
+      <input
+        id="itemName"
+        name="itemName"
+        type="text"
+        placeholder="add item ..."
+        required={true}
+        disabled={saveTransition}
+        className="md:w-2/3 rounded border border-gray-200 text-sm placeholder:text-gray-500 disabled:cursor-not-allowed"
+      />
+      <input
+        id="size"
+        name="size"
+        type="number"
+        placeholder="Size"
+        required={true}
+        disabled={saveTransition}
+        className="w-20 ml-15 rounded-md border border-gray-200 p-1 text-sm placeholder:text-gray-500 disabled:cursor-not-allowed"
+      />
+      <select
+        id="unit"
+        name="unit"
+        className="w-20 rounded border border-gray-200 text-sm placeholder:text-gray-500 disabled:cursor-not-allowed"
+        required={true}
+        disabled={saveTransition}
+        defaultValue=""
+      >
+        <option value="" disabled>
+          select
+        </option>
+        {units.map((unit) => (
+          <option key={unit} value={unit}>
+            {unit}
+          </option>
+        ))}
+      </select>
+      <input
+        id="quantity"
+        name="quantity"
+        type="number"
+        placeholder="Qty"
+        required={true}
+        disabled={saveTransition}
+        className="w-20 rounded-md border border-gray-200 text-sm placeholder:text-gray-500 disabled:cursor-not-allowed"
+      />
+      <button
+        disabled={saveTransition}
+        type="submit"
+        className="w-1/12 disabled:cursor-not-allowed"
+      >
+        <AddToCartIcon className="stroke-slate-50 hover:bg-blue-300 w-10 h-8 rounded disabled:cursor-not-allowed" />
+      </button>
+    </form>
+  );
 }
