60490f3a85c5937b3a7ac131c24f23cbaaa514a5
Sidath Samarasinghe <sidaath@gmail.com>
2024-09-14 17:34:52 +0200

    center checklist item text

--- src/app/components/checklist/CheckListItem.tsx
+++ src/app/components/checklist/CheckListItem.tsx
@@ -54,12 +54,12 @@ export default function CheckListItem({ item }: { item: CheckListItemType }) {
           label={item.itemName}
         />
       </Grid2>
-      <Grid2 size={3}>
+      <Grid2 size={3} alignContent={"center"}>
         <Typography variant="subtitle2">
           {`${item.size}  ${item.units}`}
         </Typography>
       </Grid2>
-      <Grid2 size={3}>
+      <Grid2 size={3} alignContent={"center"}>
         <Typography variant="subtitle2">{item.quantity}</Typography>
       </Grid2>
       <Grid2 size={2}>
