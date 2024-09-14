0073a617a76fafbbe0bf28922e4dbaf8bc5fab07
Sidath Samarasinghe <sidaath@gmail.com>
2024-09-14 16:55:56 +0200

    cleanup checklist component

--- src/app/components/checklist/CheckList.tsx
+++ src/app/components/checklist/CheckList.tsx
@@ -1,33 +1,47 @@
-import { Grid2, Paper, Box, Typography } from "@mui/material";
+import { Grid2, Typography } from "@mui/material";
 import { getChecklist } from "../../../server/food/checklist";
 import CheckListItem from "./CheckListItem";
 
 export default async function CheckList() {
   const data = await getChecklist();
 
-  return (
-    <Grid2
-      container
-      sx={{ height: "100%", width: "100%", overflowY: "auto" }}
-      marginTop={3}
-      alignContent={"start"}
-    >
-      <Grid2 size={4}>
-        <Typography variant="h6">Item</Typography>
+  if (data.length > 0) {
+    return (
+      <Grid2
+        container
+        sx={{ height: "100%", width: "100%", overflowY: "auto" }}
+        marginTop={3}
+        alignContent={"start"}
+      >
+        <Grid2 size={4}>
+          <Typography variant="h6">Item</Typography>
+        </Grid2>
+        <Grid2 size={3}>
+          <Typography variant="h6">Size</Typography>
+        </Grid2>
+        <Grid2 size={3}>
+          <Typography variant="h6">Quantity</Typography>
+        </Grid2>
+        {data.map((item) => {
+          return (
+            <Grid2 size={12} key={item.id}>
+              <CheckListItem item={item} />
+            </Grid2>
+          );
+        })}
       </Grid2>
-      <Grid2 size={3}>
-        <Typography variant="h6">Size</Typography>
+    );
+  } else {
+    return (
+      <Grid2
+        container
+        sx={{ height: "100%", width: "100%", overflowY: "auto" }}
+        marginTop={3}
+        justifyContent={"center"}
+        alignContent={"center"}
+      >
+        <Typography>NO ITEMS TO DISPLAY</Typography>
       </Grid2>
-      <Grid2 size={3}>
-        <Typography variant="h6">Quantity</Typography>
-      </Grid2>
-      {data.map((item) => {
-        return (
-          <Grid2 size={12} key={item.id}>
-            <CheckListItem item={item} />
-          </Grid2>
-        );
-      })}
-    </Grid2>
-  );
+    );
+  }
 }
